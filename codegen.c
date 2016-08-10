55611fd8100cd2793263b3ead19a9105a1b05398
Douglas Christman <DouglasChristman@gmail.com>
2016-08-10 14:48:36 -0400

    Remove unused headers

--- codegen.c
+++ codegen.c
@@ -6,8 +6,6 @@
 #include <string.h>
 
 #include <llvm-c/Core.h>
-#include <llvm-c/ExecutionEngine.h>
-#include <llvm-c/Target.h>
 #include <llvm-c/Analysis.h>
 #include <llvm-c/BitWriter.h>
 #include <llvm-c/Transforms/Scalar.h>
