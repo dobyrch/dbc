d0d41c1c4f5880b436a0582b92b988b392d80659
Douglas Christman <DouglasChristman@gmail.com>
2016-09-08 10:46:13 -0400

    Add extra element to array

--- llvm_bug/example.c
+++ llvm_bug/example.c
@@ -9,14 +9,10 @@
  *
  *	; ModuleID = 'example'
  *
- *	@global_int = global i64 42
- *	@global_array = global [1 x i64] [[1 x i64] ptrtoint (i64* @global_int to i64)]
+ *	@global_int = global i64 31
+ *	@global_array = global [2 x i64] [[2 x i64] 42, [2 x i64] ptrtoint (i64* @global_int to i64)]
  *	Global variable initializer type does not match global variable type!
- *	[1 x i64]* @global_array*
- *
- * Note that the return type of ptrtoint is [1 x i64], not i64.  If the
- * output is manually edited to change the type of ptrtoint to i64, llc
- * compiles it without error.
+ *	[2 x i64]* @global_array
  */
 
 #include <stddef.h>
@@ -24,25 +20,26 @@
 #include <llvm-c/Analysis.h>
 
 #define TYPE_INT LLVMInt64Type()
-#define TYPE_ARRAY LLVMArrayType(TYPE_INT, 1)
+#define TYPE_ARRAY LLVMArrayType(TYPE_INT, 2)
 
 int main(void)
 {
 	LLVMModuleRef module;
 	LLVMValueRef global_int, global_array;
 	LLVMValueRef int_init, array_init;
-	LLVMValueRef int_ptr;
+	LLVMValueRef array_vals[2];
 
 	module = LLVMModuleCreateWithName("example");
 
 	global_int = LLVMAddGlobal(module, TYPE_INT, "global_int");
 	global_array = LLVMAddGlobal(module, TYPE_ARRAY, "global_array");
 
-	int_init = LLVMConstInt(TYPE_INT, 42, 0);
+	int_init = LLVMConstInt(TYPE_INT, 31, 0);
 	LLVMSetInitializer(global_int, int_init);
 
-	int_ptr = LLVMConstPtrToInt(global_int, TYPE_INT);
-	array_init = LLVMConstArray(TYPE_ARRAY, &int_ptr, 1);
+	array_vals[0] = LLVMConstInt(TYPE_INT, 42, 0);
+	array_vals[1] = LLVMConstPtrToInt(global_int, TYPE_INT);
+	array_init = LLVMConstArray(TYPE_ARRAY, array_vals, 2);
 	LLVMSetInitializer(global_array, array_init);
 
 	LLVMDumpModule(module);
