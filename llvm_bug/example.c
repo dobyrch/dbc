0de427bda4dcc7ad3cd63cb117d805ada3c747ac
Douglas Christman <DouglasChristman@gmail.com>
2016-09-08 09:42:55 -0400

    Simplify example

--- llvm_bug/example.c
+++ llvm_bug/example.c
@@ -1,50 +1,49 @@
 /*
-Running with
-
-	clang -c example.c $(llvm-config --cflags)
-	clang++ example.o -o example $(llvm-config --ldflags --libs --system-libs)
-	./example
-
-outputs
-
-	; ModuleID = 'example'
-
-	@array_one = global [1 x i64] [i64 42]
-	@array_two = global [1 x i64] [[1 x i64] ptrtoint ([1 x i64]* @array_one to i64)]
-	Global variable initializer type does not match global variable type!
-	[1 x i64]* @array_two
-
-Note that the output type of ptrtoint is [1 x i64], not i64.  If the
-output is manually edited to change the type of ptrtoint to i64, llc
-compiles it without error.
-*/
+ * Running with
+ *
+ *	clang -c example.c $(llvm-config --cflags)
+ *	clang++ example.o -o example $(llvm-config --ldflags --libs --system-libs)
+ *	./example
+ *
+ * outputs
+ *
+ *	; ModuleID = 'example'
+ *
+ *	@global_int = global i64 42
+ *	@global_array = global [1 x i64] [[1 x i64] ptrtoint (i64* @global_int to i64)]
+ *	Global variable initializer type does not match global variable type!
+ *	[1 x i64]* @global_array*
+ *
+ * Note that the return type of ptrtoint is [1 x i64], not i64.  If the
+ * output is manually edited to change the type of ptrtoint to i64, llc
+ * compiles it without error.
+ */
 
 #include <stddef.h>
 #include <llvm-c/Core.h>
 #include <llvm-c/Analysis.h>
 
 #define TYPE_INT LLVMInt64Type()
-#define TYPE_ARRAY(n) LLVMArrayType(TYPE_INT, (n))
+#define TYPE_ARRAY LLVMArrayType(TYPE_INT, 1)
 
 int main(void)
 {
 	LLVMModuleRef module;
-	LLVMValueRef array_one, array_two;
-	LLVMValueRef init_one, init_two;
-	LLVMValueRef val_one, val_two;
+	LLVMValueRef global_int, global_array;
+	LLVMValueRef int_init, array_init;
+	LLVMValueRef int_ptr;
 
 	module = LLVMModuleCreateWithName("example");
 
-	array_one = LLVMAddGlobal(module, TYPE_ARRAY(1), "array_one");
-	array_two = LLVMAddGlobal(module, TYPE_ARRAY(1), "array_two");
+	global_int = LLVMAddGlobal(module, TYPE_INT, "global_int");
+	global_array = LLVMAddGlobal(module, TYPE_ARRAY, "global_array");
 
-	val_one = LLVMConstInt(TYPE_INT, 42, 0);
-	init_one = LLVMConstArray(TYPE_ARRAY(1), &val_one, 1);
-	LLVMSetInitializer(array_one, init_one);
+	int_init = LLVMConstInt(TYPE_INT, 42, 0);
+	LLVMSetInitializer(global_int, int_init);
 
-	val_two = LLVMConstPtrToInt(array_one, TYPE_INT);
-	init_two = LLVMConstArray(TYPE_ARRAY(1), &val_two, 1);
-	LLVMSetInitializer(array_two, init_two);
+	int_ptr = LLVMConstPtrToInt(global_int, TYPE_INT);
+	array_init = LLVMConstArray(TYPE_ARRAY, &int_ptr, 1);
+	LLVMSetInitializer(global_array, array_init);
 
 	LLVMDumpModule(module);
 	LLVMVerifyModule(module, LLVMPrintMessageAction, NULL);
